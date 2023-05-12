2e225b55ae7fb76f3011aa37fa0986e176ce1b8f
Pipejesus <grzegorz.karol.jasinski@gmail.com>
2023-05-13 00:56:54 +0200

    yeha

--- circlz.shader.glsl
+++ circlz.shader.glsl
@@ -1,3 +1,5 @@
+precision mediump float;
+
 //  Function from Iñigo Quiles
 //  https://www.shadertoy.com/view/MsS3Wc
 vec3 hsb2rgb( in vec3 c ) {
@@ -13,46 +15,38 @@ vec3 createCircular(vec2 uvc, vec3 initial, float radius, vec3 color, float seed
     vec2 dist = uvc;
     vec2 dist2 = uvc;
 
-    float factorX = pow(cos( (uvc.y + iTime + seed) / 1.2), 8.0) / 8.0;
-    float factorY = pow(sin( (uvc.x + iTime + seed) / 1.2), 8.0) / 8.0;
+    float a = atan(uvc.y, uvc.x) / 1.0;
+    float c = cos((iTime + seed + uvc.y) / 1.2);    
+    float s = sin((iTime + seed + uvc.x * a) / 1.2);
 
-    dist2.x += 0.9*factorX; 
-    dist2.y += .7 * factorY;
+    // Why? I used pow(c, 8.0) but the results were buggy on one hardware
+    // and correct on another one... no clue, missing something big here.
+    float addX = (c * c * c * c * c * c * c * c) / 8.0;
+    float addY = (s * s * s * s * s * s * s * s) / 8.0;
 
+    dist2 += vec2(0.9 * addX, 0.7 * addY);        
     radius += (0.05) * (radius / 0.5) *  (sin(iTime) + 1.0);
 
-    float z = 1.-smoothstep(radius-(radius*0.259),
-                         radius+(radius*0.259),
+    float z = 1.0 - smoothstep(radius-(radius*0.191),
+                         radius+(radius*0.041),
                          dot(dist, dist2)*4.0);
     
-    float za = smoothstep(0.5, 1.0, mod(gl_FragCoord.x, 40.0));
-    za *= smoothstep(0.5, 1.0, mod(gl_FragCoord.y, 40.0));
-
-    float za2 = (0.1 * floor(gl_FragCoord.y / 40.0));
-    za2 = za2 * (0.1 * floor(gl_FragCoord.x / 40.0));
-    
-    // if (za <= 0.0) {
-    //     color.z = color.z + 0.1;
-    // }
-    // } else {
-        // color.z = color.z * za2;
-    // }
-
     vec3 colorRGB= hsb2rgb(color);
-    vec3 r = mix(initial, colorRGB, z); // vec3 r = colorRGB * z;
+    vec3 r = mix(initial, colorRGB, z);     
     
     return mix(initial, r, z);
 }
 
 void main() {
     vec2 uv = 1.0 - (gl_FragCoord.xy / iResolution.xy);
-    vec2 uvc = uv - vec2(0.5,0.5);
+    vec2 uvc = uv - vec2(0.5,0.5);    
     uvc.x *= iResolution.x / iResolution.y;
     vec3 initial = hsb2rgb(vec3(1.0, 0.0, 0.9));
 
     float hue = 1.0; //0.113; // 0.583;
-    vec3 c = createCircular(uvc, initial, 0.52, vec3(hue, 0.03, 0.899), 3.14 / 8.0); 
-    
+    // vec3 c = createCircular(uvc, initial, 0.5, vec3(hue, 0.6, 1.0), 0.0);
+
+    vec3 c = createCircular(uvc, initial, 0.52, vec3(hue, 0.03, 0.899), 3.14 / 8.0);     
     c = createCircular(uvc, c, 0.5, vec3(hue, 0.6, 1.0), 0.0);
     c = createCircular(uvc, c, 0.35, vec3(hue, 0.5, 1.0), 3.14 / 2.0);
     c = createCircular(uvc, c, 0.05, vec3(hue, 0.4, 1.0), 3.14 / 2.0);
