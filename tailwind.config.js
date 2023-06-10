92cd3f9dc779ef739939959af7fbe26be4845ff9
Pipejesus <grzegorz.karol.jasinski@gmail.com>
2023-06-11 00:39:52 +0200

    start fixing mobile version layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,18 @@ export default {
         '3xl': '1536px',
         '4xl': '1920px'
       },
+      animation: {
+        'turntable': 'turntable 7.2s ease-in-out infinite both'
+      },
+      keyframes: {
+        turntable: {
+          '0%': { transform: 'rotateZ(0deg) scale(1.0)' },
+          '25%': { transform: 'rotateZ(90deg) scale(0.95)' },
+          '50%': { transform: 'rotateZ(180deg) scale(1.0)' },
+          '75%': { transform: 'rotateZ(270deg) scale(0.95)' },
+          '100%': { transform: 'rotateZ(360deg) scale(1.0)' },
+        }
+      },     
       gridTemplateRows: {        
         '12': 'repeat(12, minmax(0, 1fr))',      
       },
